adcdb20df392a5a5c76788d7aac5b92abd955745
Amirkhan <dddddsasq@gmail.com>
2025-03-10 00:46:46 +0500

    Refactor: Moved HotelbookerApplication.java, added controllers, repositories, and services

--- src/main/java/com/hotelbooker/HotelbookerApplication.java
+++ src/main/java/com/hotelbooker/HotelbookerApplication.java
@@ -1,4 +1,4 @@
-package com.hotelbooker.hotelbooker;
+package com.hotelbooker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
